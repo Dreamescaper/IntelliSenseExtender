257ebb3f1e50c294b801dac9d00829cb115b7c67
oleksandr Liakhevych <Oleksandr_Liakhevych@epam.com>
2017-03-22 23:03:16 +0200

    Remove wrong copyright

--- IntelliSenseExtender/IntelliSenseExtenderPackage.cs
+++ IntelliSenseExtender/IntelliSenseExtenderPackage.cs
@@ -1,10 +1,4 @@
-﻿//------------------------------------------------------------------------------
-// <copyright file="IntelliSenseExtenderPackage.cs" company="EPAM Systems">
-//     Copyright (c) EPAM Systems.  All rights reserved.
-// </copyright>
-//------------------------------------------------------------------------------
-
-using System;
+﻿using System;
 using System.Runtime.InteropServices;
 using IntelliSenseExtender.Options;
 using Microsoft.VisualStudio;
@@ -15,20 +9,6 @@ namespace IntelliSenseExtender
     /// <summary>
     /// This is the class that implements the package exposed by this assembly.
     /// </summary>
-    /// <remarks>
-    /// <para>
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the
-    /// IVsPackage interface and uses the registration attributes defined in the framework to
-    /// register itself and its components with the shell. These attributes tell the pkgdef creation
-    /// utility what data to put into .pkgdef file.
-    /// </para>
-    /// <para>
-    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
-    /// </para>
-    /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
     [Guid("718335A4-0E73-4834-A43E-E64DECE87EAB")]
@@ -50,10 +30,6 @@ namespace IntelliSenseExtender
         /// </summary>
         public IntelliSenseExtenderPackage()
         {
-            // Inside this method you can place any initialization code that does not require
-            // any Visual Studio service because at this point the package object is created but
-            // not sited yet inside Visual Studio environment. The place to do all the other
-            // initialization is the Initialize method.
         }
 
         public OptionsPage GetOptionsPage()
@@ -61,15 +37,9 @@ namespace IntelliSenseExtender
             return (OptionsPage)GetDialogPage(typeof(OptionsPage));
         }
 
-
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
         protected override void Initialize()
         {
-            OptionsProvider.GetOptionsPageFunc = () => GetOptionsPage();
-
+            OptionsProvider.GetOptionsPageFunc = GetOptionsPage;
             base.Initialize();
         }
     }
