1c728321f884109452e5491426be426d0dd1b948
oleksandr Liakhevych <Oleksandr_Liakhevych@epam.com>
2017-04-03 17:36:20 +0300

    Fix methods not suggested for generic types

--- IntelliSenseExtender/Extensions/SymbolExtensions.cs
+++ IntelliSenseExtender/Extensions/SymbolExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.CodeAnalysis;
 
 namespace IntelliSenseExtender.Extensions
@@ -15,19 +16,17 @@ namespace IntelliSenseExtender.Extensions
             return symbol.ToDisplayString();
         }
 
-        public static bool IsAssignableFrom(this ITypeSymbol baseTypeSymbol, ITypeSymbol derivedSymbol)
+        public static bool IsAssignableFrom(this ITypeSymbol baseSymbol, ITypeSymbol derivedSymbol)
         {
-            var currentTypeSymbol = derivedSymbol;
-            while (currentTypeSymbol != null)
+            if (!(baseSymbol is INamedTypeSymbol baseTypeSymbol
+                && derivedSymbol is INamedTypeSymbol derivedTypeSymbol))
             {
-                if (currentTypeSymbol == baseTypeSymbol
-                    || currentTypeSymbol.Interfaces.Any(i => baseTypeSymbol == i))
-                {
-                    return true;
-                }
-                currentTypeSymbol = currentTypeSymbol.BaseType;
+                return false;
             }
-            return false;
+
+            return GetAssignableTypeSymbols(derivedTypeSymbol)
+                .Any(ts => ts.MetadataName == baseTypeSymbol.MetadataName
+                    && (!ts.IsGenericType || IsGenericTypeParametersCompatible(baseTypeSymbol, ts)));
         }
 
         public static bool IsAttribute(this INamedTypeSymbol typeSymbol)
@@ -44,6 +43,48 @@ namespace IntelliSenseExtender.Extensions
                 currentSymbol = currentSymbol.BaseType;
             }
             return false;
+        }
+
+        private static IEnumerable<INamedTypeSymbol> GetAssignableTypeSymbols(INamedTypeSymbol currentTypeSymbol)
+        {
+            while (currentTypeSymbol != null)
+            {
+                yield return currentTypeSymbol;
+                foreach (var i in currentTypeSymbol.Interfaces)
+                {
+                    yield return i;
+                }
+                currentTypeSymbol = currentTypeSymbol.BaseType;
+            }
+        }
+
+        /// <summary>
+        /// Returns true if we can provide derivedTypeSymbol as parameter for baseTypeSymbol parameter type.
+        /// </summary>
+        private static bool IsGenericTypeParametersCompatible(INamedTypeSymbol baseTypeSymbol, INamedTypeSymbol derivedTypeSymbol)
+        {
+            if (baseTypeSymbol.Arity != derivedTypeSymbol.Arity)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < baseTypeSymbol.Arity; i++)
+            {
+                var baseTypeArgument = baseTypeSymbol.TypeArguments[i];
+                var derivedTypeArgument = derivedTypeSymbol.TypeArguments[i];
+
+                // generic 'type' has NotApplicable accessability
+                // TODO: it seems to be working, but looks strange. Find better approach
+                // TODO: add support for 'where' conditions
+                bool compatible = baseTypeArgument.DeclaredAccessibility == Accessibility.NotApplicable
+                    || baseTypeArgument == derivedTypeArgument;
+                if (!compatible)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
