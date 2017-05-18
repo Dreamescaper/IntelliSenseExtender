20c03f2d5b36ad8878326de5d1a19da3445f9c80
oleksandr Liakhevych <Oleksandr_Liakhevych@epam.com>
2017-05-19 00:36:40 +0300

    Add basic unit test coverage for UnimportedCompletionProvider

---
+++ IntelliSenseExtender.Tests/CompletionProviders/AbstractCompletionProviderTest.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using IntelliSenseExtender.Options;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Completion;
+using Microsoft.CodeAnalysis.Text;
+
+namespace IntelliSenseExtender.Tests.CompletionProviders
+{
+    public abstract class AbstractCompletionProviderTest
+    {
+        private static MetadataReference Mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+        private static MetadataReference SystemCore = MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location);
+
+        #region Options
+
+        public static OptionsProvider Options_Default => new OptionsProvider(
+            new Options.Options
+            {
+                EnableExtensionMethodsSuggestions = true,
+                EnableTypesSuggestions = true,
+                FilterOutObsoleteSymbols = true,
+                SortCompletionsAfterImported = true,
+                UserCodeOnlySuggestions = false
+            });
+
+        public static OptionsProvider Options_ExtensionMethodsOnly => new OptionsProvider(
+            new Options.Options
+            {
+                EnableExtensionMethodsSuggestions = true,
+                EnableTypesSuggestions = false,
+                FilterOutObsoleteSymbols = true,
+                SortCompletionsAfterImported = true,
+                UserCodeOnlySuggestions = false
+            });
+
+        public static OptionsProvider Options_TypesOnly => new OptionsProvider(
+            new Options.Options
+            {
+                EnableExtensionMethodsSuggestions = false,
+                EnableTypesSuggestions = true,
+                FilterOutObsoleteSymbols = true,
+                SortCompletionsAfterImported = true,
+                UserCodeOnlySuggestions = false
+            });
+
+        #endregion
+
+        public static Document GetTestDocument(string source)
+        {
+            return GetTestDocument(source, new string[] { });
+        }
+
+        public static Document GetTestDocument(string source, params string[] additionalFiles)
+        {
+            ProjectId projectId = ProjectId.CreateNewId();
+            DocumentId documentId = DocumentId.CreateNewId(projectId);
+
+            var solution = new AdhocWorkspace().CurrentSolution
+                .AddProject(projectId, "MyProject", "MyProject", LanguageNames.CSharp)
+                .AddMetadataReference(projectId, Mscorlib)
+                .AddMetadataReference(projectId, SystemCore)
+                .AddDocument(documentId, "MyFile.cs", source);
+
+            for (int i = 0; i < additionalFiles.Length; i++)
+            {
+                solution = solution.AddDocument(DocumentId.CreateNewId(projectId), $"{i}.cs", additionalFiles[i]);
+            }
+
+            var document = solution.GetDocument(documentId);
+
+            return document;
+        }
+
+        public static CompletionContext GetContext(Document document, CompletionProvider provider, int position)
+        {
+            var context = new CompletionContext(provider, document, position,
+                new TextSpan(), CompletionTrigger.Default, document.Project.Solution.Workspace.Options,
+                default(CancellationToken));
+            return context;
+        }
+
+        public static IReadOnlyList<CompletionItem> GetCompletions(CompletionContext context)
+        {
+            var property = context.GetType().GetProperty("Items", BindingFlags.Instance | BindingFlags.NonPublic);
+            return (IReadOnlyList<CompletionItem>)property.GetValue(context);
+        }
+
+        public static IReadOnlyList<CompletionItem> GetCompletions(CompletionProvider provider, string source, string additinalFile, string searchText)
+        {
+            int position = source.IndexOf(searchText) + searchText.Length;
+
+            var document = GetTestDocument(source, additinalFile);
+            var completionContext = GetContext(document, provider, position);
+            provider.ProvideCompletionsAsync(completionContext).Wait();
+            var completions = GetCompletions(completionContext);
+            return completions;
+        }
+
+        public static IReadOnlyList<CompletionItem> GetCompletions(CompletionProvider provider, string source, string searchText)
+        {
+            int position = source.IndexOf(searchText) + searchText.Length;
+
+            var document = GetTestDocument(source);
+            var completionContext = GetContext(document, provider, position);
+            provider.ProvideCompletionsAsync(completionContext).Wait();
+            var completions = GetCompletions(completionContext);
+            return completions;
+        }
+
+        public class OptionsProvider : IOptionsProvider
+        {
+            private readonly Options.Options _options;
+
+            public OptionsProvider(Options.Options options)
+            {
+                _options = options;
+            }
+
+            public Options.Options GetOptions()
+            {
+                return _options;
+            }
+        }
+    }
+}
---
+++ IntelliSenseExtender.Tests/CompletionProviders/UnimportedExtensionMethodsTests.cs
@@ -0,0 +1,253 @@
+﻿using System.Linq;
+using IntelliSenseExtender.IntelliSense.Providers;
+using NUnit.Framework;
+
+namespace IntelliSenseExtender.Tests.CompletionProviders
+{
+    [TestFixture]
+    public class UnimportedCompletionProviderTests : AbstractCompletionProviderTest
+    {
+        #region Types
+
+        [Test]
+        public void Types_ProvideReferencesCompletions_List()
+        {
+            var source = @"
+                public class Test {
+                    public void Method() {
+                        var list = new 
+                    }
+                }";
+
+            var provider = new UnimportedCSharpCompletionProvider(Options_Default);
+            var completions = GetCompletions(provider, source, "var list = new ");
+            var completionsNames = completions.Select(completion => completion.DisplayText);
+            Assert.That(completionsNames, Does.Contain("List<>  (System.Collections.Generic)"));
+        }
+
+        [Test]
+        public void Types_ProvideUserCodeCompletions()
+        {
+            var mainSource = @"
+                public class Test {
+                    public void Method() {
+                        /*here*/
+                    }
+                }";
+            var classFile = @"
+                namespace NM
+                {
+                    public class Class
+                    {
+                    }
+                }";
+
+            var provider = new UnimportedCSharpCompletionProvider(Options_Default);
+            var completions = GetCompletions(provider, mainSource, classFile, "/*here*/");
+            var completionsNames = completions.Select(completion => completion.DisplayText);
+            Assert.That(completionsNames, Does.Contain("Class  (NM)"));
+        }
+
+        [Test]
+        public void Types_DoNotProvideCompletionsIfTypeNotExpected()
+        {
+            var mainSource = @"
+                public /*0*/ class Test {
+                    public void /*1*/ Method() {
+                        
+                    }
+                }";
+            var classFile = @"
+                namespace NM
+                {
+                    public class Class
+                    {
+                    }
+                }";
+
+            var provider = new UnimportedCSharpCompletionProvider(Options_TypesOnly);
+
+            for (int i = 0; i < 3; i++)
+            {
+                var completions = GetCompletions(provider, mainSource, classFile, $"/*{i}*/");
+                Assert.That(completions, Is.Empty);
+            }
+        }
+
+        #endregion
+
+        #region Extension Methods
+
+        [Test]
+        public void ExtensionMethods_ProvideReferencesCompletions_Linq()
+        {
+            var source = @"
+                using System.Collections.Generic;
+                public class Test {
+                    public void Method() {
+                        var list = new List<string>();
+                        list.
+                    }
+                }";
+
+            var provider = new UnimportedCSharpCompletionProvider(Options_Default);
+            var completions = GetCompletions(provider, source, "list.");
+            var completionsNames = completions.Select(completion => completion.DisplayText);
+            Assert.That(completionsNames, Does.Contain("Select<>  (System.Linq)"));
+        }
+
+        [Test]
+        public void ExtensionMethods_ProvideUserCodeCompletions()
+        {
+            var mainSource = @"
+                public class Test {
+                    public void Method() {
+                        object obj = null;
+                        obj.
+                    }
+                }";
+            var extensionsFile = @"
+                namespace NM
+                {
+                    public static class ObjectExtensions
+                    {
+                        public static void Do(this object var)
+                        { }
+                    }
+                }";
+
+            var provider = new UnimportedCSharpCompletionProvider(Options_Default);
+            var completions = GetCompletions(provider, mainSource, extensionsFile, "obj.");
+            var completionsNames = completions.Select(completion => completion.DisplayText);
+            Assert.That(completionsNames, Does.Contain("Do  (NM)"));
+        }
+
+        [Test]
+        public void ExtensionMethods_ProvideCompletionsForLiterals()
+        {
+            var mainSource = @"
+                public class Test {
+                    public void Method() {
+                        111.
+                    }
+                }";
+            var extensionsFile = @"
+                namespace NM
+                {
+                    public static class ObjectExtensions
+                    {
+                        public static void Do(this object var)
+                        { }
+                    }
+                }";
+
+            var provider = new UnimportedCSharpCompletionProvider(Options_Default);
+            var completions = GetCompletions(provider, mainSource, extensionsFile, "111.");
+            var completionsNames = completions.Select(completion => completion.DisplayText);
+            Assert.That(completionsNames, Does.Contain("Do  (NM)"));
+        }
+
+        [Test]
+        public void ExtensionMethods_DoNotProvideCompletionsIfMemberIsNotAccessed()
+        {
+            var source = @"
+                using System;
+                namespace A{
+                    class CA
+                    {
+                        [System.Obsolete]
+                        public void MA(int par)
+                        {
+                            var a = 0;
+                        }
+                    }
+                }
+                namespace B{
+                    static class B{
+                        public static void ExtIntM(this int par)
+                        { }
+                    }
+                }";
+            var extensionsFile = @"
+                namespace NM
+                {
+                    public static class ObjectExtensions
+                    {
+                        public static void Do(this object var)
+                        { }
+                    }
+                }";
+
+            var provider = new UnimportedCSharpCompletionProvider(Options_ExtensionMethodsOnly);
+            var document = GetTestDocument(source, extensionsFile);
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                var context = GetContext(document, provider, i);
+                provider.ProvideCompletionsAsync(context).Wait();
+                var completions = GetCompletions(context);
+
+                Assert.That(completions, Is.Empty);
+            }
+        }
+
+        [Test]
+        public void ExtensionMethods_DoNotProvideCompletionsWhenTypeIsAccessed()
+        {
+            var mainSource = @"
+                public class Test {
+                    public void Method() {
+                        object.
+                    }
+                }";
+            var extensionsFile = @"
+                namespace NM
+                {
+                    public static class ObjectExtensions
+                    {
+                        public static void Do(this object var)
+                        { }
+                    }
+                }";
+
+            var provider = new UnimportedCSharpCompletionProvider(Options_ExtensionMethodsOnly);
+            var completions = GetCompletions(provider, mainSource, extensionsFile, "object.");
+            Assert.That(completions, Is.Empty);
+        }
+
+        [Test]
+        public void ExtensionMethods_DoNotProvideObsolete()
+        {
+            var mainSource = @"
+                public class Test {
+                    public void Method() {
+                        object.
+                    }
+                }";
+            var extensionsFile = @"
+                namespace NM
+                {
+                    [System.Obsolete]
+                    public static class ObjectExtensions1
+                    {
+                        public static void Do1(this object var)
+                        { }
+                    }
+
+                    public static class ObjectExtensions2
+                    {
+                        [System.Obsolete]
+                        public static void Do2(this object var)
+                        { }
+                    }
+                }";
+
+            var provider = new UnimportedCSharpCompletionProvider(Options_ExtensionMethodsOnly);
+            var completions = GetCompletions(provider, mainSource, extensionsFile, "object.");
+            Assert.That(completions, Does.Not.Contain("Do1  (NM)"));
+            Assert.That(completions, Does.Not.Contain("Do2  (NM)"));
+        }
+
+        #endregion
+    }
+}
