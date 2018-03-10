b9b0212dca560207b401275fca475b32b2879396
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2018-03-10 03:11:39 +0200

    Add failing test for adding import inside namespace

--- IntelliSenseExtender.Tests/CompletionProviders/UtilsTests.cs
+++ IntelliSenseExtender.Tests/CompletionProviders/UtilsTests.cs
@@ -24,5 +24,27 @@ namespace IntelliSenseExtender.Tests.CompletionProviders
 
             Assert.That(newDocText, Does.Contain("using System;"));
         }
+
+        [Test]
+        public void NamespaceResolver_ShouldAddUsingInsideNamespaceIfUsingsArePresent()
+        {
+            // Basically verify that all that reflection crap is not broken.
+
+            const string source = @"
+                namespace ns.something
+                {
+                    using System;
+
+                    public class Test {}
+                }";
+
+            var document = GetTestDocument(source);
+            var newDoc = new NamespaceResolver().AddNamespaceImport("System.Collections", document, CancellationToken.None).Result;
+            var newDocText = newDoc.GetTextAsync().Result.ToString();
+
+            Assert.That(newDocText, Does.Contain("using System.Collections;"));
+            Assert.That(newDocText.IndexOf("using System.Collections;"),
+                Is.GreaterThan(newDocText.IndexOf("namespace ns.something")));
+        }
     }
 }
