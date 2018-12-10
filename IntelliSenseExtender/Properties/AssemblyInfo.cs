66f61049720b70b6a1a1f37bec4c290e7f3a3732
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2018-12-10 15:57:56 +0200

    Minor csproj cleanup

--- IntelliSenseExtender/Properties/AssemblyInfo.cs
+++ IntelliSenseExtender/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IntelliSenseSolutionWide")]
+[assembly: AssemblyTitle("IntelliSenseExtender")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IntelliSenseSolutionWide")]
+[assembly: AssemblyProduct("IntelliSenseExtender")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
