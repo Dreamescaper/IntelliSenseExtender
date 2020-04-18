ae83a2fced8adbd18467641ffb8f9a4af3fef5ce
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2020-04-18 03:06:34 +0300

    VS 16.6P3 fix

--- IntelliSenseExtender/ExposedInternals/GenerationOptions.cs
+++ IntelliSenseExtender/ExposedInternals/GenerationOptions.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Linq;
 using Microsoft.CodeAnalysis.Options;
 
 #nullable disable
+#pragma warning disable REFL009 // The referenced member is not known to exist.
 
 namespace IntelliSenseExtender.ExposedInternals
 {
@@ -15,7 +17,26 @@ namespace IntelliSenseExtender.ExposedInternals
             var workspacesAssembly = AppDomain.CurrentDomain.GetAssemblies()
                 .First(a => a.GetName().Name == "Microsoft.CodeAnalysis.Workspaces");
             var type = workspacesAssembly.GetType("Microsoft.CodeAnalysis.Editing.GenerationOptions");
-            PlaceSystemNamespaceFirst = (PerLanguageOption<bool>)type?.GetField("PlaceSystemNamespaceFirst")?.GetValue(null);
+            var option = type?.GetField("PlaceSystemNamespaceFirst")?.GetValue(null);
+
+            if (option is PerLanguageOption<bool> oldOption)
+            {
+                PlaceSystemNamespaceFirst = oldOption;
+            }
+            else
+            {
+                // VS 16.6P3
+                // option is PerLanguageOption2<bool>
+
+                var optionType = option.GetType();
+                var storageLocations = optionType.GetProperty("StorageLocations").GetValue(option) as IEnumerable;
+
+                PlaceSystemNamespaceFirst = new PerLanguageOption<bool>(
+                    nameof(GenerationOptions),
+                    nameof(PlaceSystemNamespaceFirst),
+                    defaultValue: true,
+                    storageLocations: storageLocations.OfType<OptionStorageLocation>().ToArray());
+            }
         }
     }
 }
