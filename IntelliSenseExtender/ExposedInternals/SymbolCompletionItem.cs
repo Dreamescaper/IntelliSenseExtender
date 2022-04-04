190e4579623099e0d5f7b35d174b933bd35d03b7
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2022-04-04 23:09:27 +0300

    VS 17.2 support

--- IntelliSenseExtender/ExposedInternals/SymbolCompletionItem.cs
+++ IntelliSenseExtender/ExposedInternals/SymbolCompletionItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
@@ -16,28 +17,52 @@ namespace IntelliSenseExtender.ExposedInternals
     public static class SymbolCompletionItem
     {
         private static readonly EncodeSymbolHandler _encodeSymbolMethod;
-        private static readonly GetDescriptionAsyncHandler _getDescriptionAsyncMethod;
+        private static readonly GetDescriptionAsyncOldHandler _getDescriptionAsyncOldMethod;
 
-        private delegate Task<CompletionDescription> GetDescriptionAsyncHandler(CompletionItem item, Document document, CancellationToken cancellationToken);
+        private static readonly MethodInfo _getDescriptionAsyncMethod;
+
+        private static object _symbolDescriptionOptionsDefault;
 
         private delegate string EncodeSymbolHandler(ISymbol symbol);
+        private delegate Task<CompletionDescription> GetDescriptionAsyncOldHandler(CompletionItem item, Document document, CancellationToken cancellationToken);
 
         static SymbolCompletionItem()
         {
             var featuresAssembly = AppDomain.CurrentDomain.GetAssemblies()
                 .First(a => a.GetName().Name == "Microsoft.CodeAnalysis.Features");
-            var _internalType = featuresAssembly.GetType("Microsoft.CodeAnalysis.Completion.Providers.SymbolCompletionItem");
+            var symbolCompletionItemType = featuresAssembly.GetType("Microsoft.CodeAnalysis.Completion.Providers.SymbolCompletionItem");
 
-            _encodeSymbolMethod = (EncodeSymbolHandler)_internalType
+            _encodeSymbolMethod = (EncodeSymbolHandler)symbolCompletionItemType
                 .GetMethod("EncodeSymbol", new[] { typeof(ISymbol) })
                 .CreateDelegate(typeof(EncodeSymbolHandler));
 
-            _getDescriptionAsyncMethod = (GetDescriptionAsyncHandler)_internalType
-                .GetMethod("GetDescriptionAsync", new[] { typeof(CompletionItem), typeof(Document), typeof(CancellationToken) })
-                .CreateDelegate(typeof(GetDescriptionAsyncHandler));
+            // Pre-17.2 P1
+            var getDescriptionOldMethod = symbolCompletionItemType
+                .GetMethod("GetDescriptionAsync", new[] { typeof(CompletionItem), typeof(Document), typeof(CancellationToken) });
+
+            if (getDescriptionOldMethod != null)
+            {
+                _getDescriptionAsyncOldMethod = (GetDescriptionAsyncOldHandler)getDescriptionOldMethod.CreateDelegate(typeof(GetDescriptionAsyncOldHandler));
+            }
+            else
+            {
+                var symbolDescriptionOptionsType = featuresAssembly.GetType("Microsoft.CodeAnalysis.LanguageServices.SymbolDescriptionOptions");
+                _symbolDescriptionOptionsDefault = symbolDescriptionOptionsType.GetField("Default").GetValue(null);
+                _getDescriptionAsyncMethod = symbolCompletionItemType.GetMethod("GetDescriptionAsync", new[] { typeof(CompletionItem), typeof(Document), symbolDescriptionOptionsType, typeof(CancellationToken) });
+            }
         }
 
-        public static Task<CompletionDescription> GetDescriptionAsync(CompletionItem item, Document document, CancellationToken cancellationToken) => _getDescriptionAsyncMethod(item, document, cancellationToken);
+        public static Task<CompletionDescription> GetDescriptionAsync(CompletionItem item, Document document, CancellationToken cancellationToken)
+        {
+            if (_getDescriptionAsyncOldMethod != null)
+            {
+                return _getDescriptionAsyncOldMethod(item, document, cancellationToken);
+            }
+            else
+            {
+                return (Task<CompletionDescription>)_getDescriptionAsyncMethod.Invoke(null, new object[] { item, document, _symbolDescriptionOptionsDefault, cancellationToken });
+            }
+        }
 
         public static string EncodeSymbol(ISymbol symbol) => _encodeSymbolMethod(symbol);
     }
